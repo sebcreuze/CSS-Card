e6d7fe24d51365fc03f47846bca8518c0e400fe3
unknown <sebastien.creuze@live.fr>
2024-04-04 11:55:46 +0200

    finish quest CSS into REACT

---
+++ src/components/App.jsx
@@ -0,0 +1,24 @@
+function App() {
+  return (
+    <>
+      <figure className="card">
+        <img
+          src="https://img.freepik.com/photos-gratuite/aquarelle-hibou-blanc-gris-aux-yeux-jaunes_78370-1782.jpg?w=826"
+          alt="Hedwige"
+          className="card-img"
+        />
+        <figcaption>Hedwige</figcaption>
+      </figure>
+      <figure className="card">
+        <img
+          src="https://images.unsplash.com/photo-1697213171386-f1743a4152e5?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="Dobby"
+          className="card-img"
+        />
+        <figcaption>Dobby</figcaption>
+      </figure>
+    </>
+  );
+}
+
+export default App;
